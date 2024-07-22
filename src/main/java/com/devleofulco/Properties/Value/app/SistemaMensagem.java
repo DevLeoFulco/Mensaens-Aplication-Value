f1ca2629cb6b52123dab23ad1bdfcef813b2d937
DevLeoFulco <devleofulco@gmail.com>
2024-07-22 00:16:34 -0300

    Uso do Configuration Properties

--- src/main/java/com/devleofulco/Properties/Value/app/SistemaMensagem.java
+++ src/main/java/com/devleofulco/Properties/Value/app/SistemaMensagem.java
@@ -1,5 +1,6 @@
 package com.devleofulco.Properties.Value.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,17 +9,13 @@ import java.util.List;
 
 @Component
 public class SistemaMensagem implements CommandLineRunner {
+    @Autowired
+    private Remetente remetente;
 
-    @Value("${nome}")
-    private String nome;
-    @Value("${email}")
-    private String email;
-    @Value("${telefones}")
-    private List<Long> telefones;
 
     @Override
     public void run(String... args)throws Exception{
-        System.out.println("Mensagem enviada por: "+ nome + "\n E-mail: "+email +"\n Com telefone: "+ telefones);
+        System.out.println("Mensagem enviada por: "+ remetente.getNome() + "\n E-mail: "+remetente.getEmail() +"\n Com telefone: "+ remetente.getTelefones());
         System.out.println("Seu cadastro foi aprovado! ");
     }
 }
